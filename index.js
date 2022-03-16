aea5568e20fe9e9860f3ab4af57587e459af9d55
Marcos Valderrabano <ing.marco.valu@gmail.com>
2022-03-15 23:25:05 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name) {
+    const copyCats = [...cats, name];
+
+    return copyCats;
+}
+
+function prependCat(name) {
+    const copyCats = [name, ...cats];
+
+    return copyCats;
+}
+function removeLastCat() {
+    return cats.slice(0, 2);
+}
+
+function removeFirstCat() {
+    return cats.slice(1);
+}
